6285c90c4ad6aac71f6d869c9fc491d108b2445e
jainam-shah-se <96842340+jainam-shah-se@users.noreply.github.com>
2022-09-02 14:18:11 -0700

    Time: 819 ms (25.24%), Space: 25.9 MB (69.11%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -5,12 +5,11 @@ class Solution:
         for num in nums:
             if num in uni:
                 return True
-                print(uni)
-                print(num)
+                
             else:
                 uni[num] = '0'
                 
-                print("Hello")
+                
         return False
             
         
