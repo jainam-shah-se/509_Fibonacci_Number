163c266e9cb9490e367a80e4b02117266888cfe4
jainam-shah-se <96842340+jainam-shah-se@users.noreply.github.com>
2022-09-05 20:14:34 -0700

    Time: 144 ms (5.27%), Space: 14.6 MB (26.84%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,41 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        ch_dict = {}
+        ch_dict2 = {}
+        
+        for i in s:
+            if i in ch_dict:
+
+                cnt = ch_dict.get(i)
+                cnt += 1
+                # print(cnt)
+                ch_dict[i] = cnt
+                
+            else:
+                
+                ch_dict[i] = 1
+            
+        for i in t:
+            if i in ch_dict2:
+
+                cnt1 = ch_dict2.get(i)
+                cnt1 += 1
+                # print(cnt)
+                ch_dict2[i] = cnt1
+                
+            else:
+                
+                ch_dict2[i] = 1
+            
+        # print(ch_dict)
+        # print(ch_dict2)
+        
+        if (ch_dict == ch_dict2):
+            return True
+        return False
+            
+        
+        
+        
+        
